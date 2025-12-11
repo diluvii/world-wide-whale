b19defe530de02bb1cdfdc132df309ef6119a063
yawen xue <124210098+yawenx2004@users.noreply.github.com>
2025-12-11 14:32:55 -0800

    made this build only

--- scripts/camera.gd
+++
@@ -1,8 +0,0 @@
-extends Camera2D
-
-@export var target: CharacterBody2D
-@export var smoothing := 8.0
-
-func _process(delta):
-	if target:
-		global_position = lerp(global_position, target.global_position, smoothing * delta)
--- scripts/player_hitbox.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	connect("area_entered", Callable(self, "_on_area_entered"))
-
-func _on_area_entered(area):
-	if area.is_in_group("enemy_hurtbox"):
-		pass
